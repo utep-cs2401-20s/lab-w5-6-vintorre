922f2900fd15ad4258ea596ce93cd7ea34fd8901
vintorre <vincent.dlt@yahoo.com>
2020-02-17 14:44:18 -0700

    added the rest of the attributes, my constructors and the beginning of my first method

--- Java/src/snakeGame.java
+++ Java/src/snakeGame.java
@@ -5,5 +5,34 @@ public class snakeGame {
 
     private boolean[][] game;
     private int[] headPosition;
+    private static int exhaustiveChecks;
+    private static int recursiveChecks;
+
+    public snakeGame(){
+        boolean[][] game = new boolean[1][1];
+    }
+
+    public snakeGame(boolean[][] g, int x, int y){
+        int sizer = g.length;
+        int sizec = g[0].length;
+
+        boolean[][] game = new boolean[sizer][sizec];
+
+        headPosition = {x,y};
+
+        for(int i = 0; i < g.length; i++){
+            for(int j = 0; j <g.length; j++){
+                game[i][j] = g[i][j];
+            }
+        }
+
+    }
+
+    public int[] findTailExhaustive(){
+        int countEx = 0;
+
+
+
+    }
 
 }
