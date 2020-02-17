e34a3f8636150d0a95ad415ef3d1f7ce117b9853
vintorre <vincent.dlt@yahoo.com>
2020-02-17 14:14:07 -0700

    added class and some attributes

---
+++ Java/src/snakeGame.java
@@ -0,0 +1,9 @@
+public class snakeGame {
+    //recursion stop when I find the tail or when there is nothing else to look for
+    //call on neighbors/ head
+    //first is cells checked second is cells checked also
+
+    private boolean[][] game;
+    private int[] headPosition;
+
+}
