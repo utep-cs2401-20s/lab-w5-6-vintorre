334d6e23b5aef1a8c6955de5a4f1612d691ce9cf
vintorre <vincent.dlt@yahoo.com>
2020-02-19 14:48:16 -0700

    added my two constructors and added a non working findTailex method

--- Java/src/snakeGame.java
+++ Java/src/snakeGame.java
@@ -16,9 +16,12 @@ public class snakeGame {
         int sizer = g.length;
         int sizec = g[0].length;
 
-        boolean[][] game = new boolean[sizer][sizec];
+        game = new boolean[sizer][sizec];
 
-        headPosition = {x,y};
+        headPosition = new int[2];
+
+        headPosition[0] = x;
+        headPosition[1] = y;
 
         for(int i = 0; i < g.length; i++){
             for(int j = 0; j <g.length; j++){
@@ -28,11 +31,74 @@ public class snakeGame {
 
     }
 
+    public void resetCounters(){
+        exhaustiveChecks = 0;
+        recursiveChecks = 0;
+    }
+
     public int[] findTailExhaustive(){
+        resetCounters();
         int countEx = 0;
+        int snakeLength = 0;
+        int [] tailPosition = new int[2];
+        int[] tailReturn = new int[3];
+
+        for(int i = 0; i < game.length; i++){
+            for(int j = 0; j < game[i].length; j++){
+                countEx++;
+                if(game[i][j] == false)
+                    continue;
+                if(game[i][j] == true) {
+                    snakeLength++;
+                    if((neighbors(i,j) == 1) && (i != headPosition[0]) && (j != headPosition[1])) {
+                        tailPosition[0] = i;
+                        tailPosition[1] = j;
+                    }
+                    if(neighbors(i, j) == 2)
+                        continue;
+
+                }
+
+            }
+        }
+
+        tailReturn[0] = tailPosition[0];
+        tailReturn[1] = tailPosition[1];
+        tailReturn[2] = snakeLength;
+        return tailReturn;
 
 
+    }
 
+    public int neighbors(int r, int c){
+        int count = 0;
+
+        if(r-1 >= 0){
+            if (this.game[r - 1][c] == true)
+                count++;
+        }
+
+        if (c - 1 >= 0)
+            if (this.game[r][c - 1] == true)
+                count++;
+
+        if(c+1< game[0].length)
+            if (this.game[r][c + 1] == true)
+                count++;
+
+        if(r+1 < game.length) {
+            if (this.game[r + 1][c] == true)
+                count++;
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        boolean[][] test = {{true, true, true, false},{true, false, true, false},{true, false, true, false},{true, false, true, false}};
+
+        snakeGame S = new snakeGame(test, 3, 0);
+        System.out.println(S.findTailExhaustive());
     }
 
 }
